d8b6aeb6a4826a81b8c5f6d39f267a26c8cbd804
Leonid <kissmykrabeskill@gmail.com>
2020-11-24 20:58:34 +0500

    HW

--- Lesson_08/src/geekbrains/home/des/HomeHashTableImpl.java
+++ Lesson_08/src/geekbrains/home/des/HomeHashTableImpl.java
@@ -1,12 +1,13 @@
 package geekbrains.home.des;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeHashTableImpl<K,V> implements  HashTable<K, V> {
 
     private final int maxSize;
 
-    private final ArrayList[] data;
+    private final List<Node> data[];
     private int size;
 
     public HomeHashTableImpl(int maxSize) {
@@ -16,6 +17,20 @@ public class HomeHashTableImpl<K,V> implements  HashTable<K, V> {
 
     @Override
     public boolean put(K key, V value) {
+        int index = hash(key);
+
+        List list = data[index];
+        Node node;
+        for (Object n: list) {
+            node = (Node) n;
+            if (node.hashCode() == key.hashCode()){
+                throw new IllegalArgumentException("Such a key already exists");
+            } else {
+                list.add(new Node<>(key, value));
+                size++;
+                return true;
+            }
+        }
         return false;
     }
 
@@ -25,6 +40,18 @@ public class HomeHashTableImpl<K,V> implements  HashTable<K, V> {
 
     @Override
     public V get(K key) {
+        int index = hash(key);
+
+        List list = data[index];
+        V node;
+
+        for (Object n: list) {
+            node = (V) n;
+            if (node.hashCode() == key.hashCode()){
+                return node;
+            }
+        }
+
         return null;
     }
 
@@ -41,6 +68,10 @@ public class HomeHashTableImpl<K,V> implements  HashTable<K, V> {
 
     @Override
     public void display() {
-
+        System.out.println("----------");
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d = [%s]%n", i, data[i]);
+        }
+        System.out.println("----------");
     }
 }
